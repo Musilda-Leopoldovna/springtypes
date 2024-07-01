61448f1f2d3b7633dbc0c663935ea634e8cadec7
Musilda-Leopoldovna <Musilda-Leopoldovna@users.noreply.github.com>
2024-07-01 20:17:54 +0200

    Completing the task

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -1,4 +1,11 @@
 package koschei.models;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Deth8 {
+    @Override
+    public String toString() {
+        return "...Сказка ложь, да в ней намёк. Не ходи на Рагнарёк.";
+    }
 }
